76d26916d25a92a2b4a4e8c9658f0b3c6b84a375
Robert Cronin <robert.cronin@gatech.edu>
2020-05-04 18:00:52 +0800

    WIP: update cli

--- bin/polykey.ts
+++ bin/polykey.ts
@@ -5,22 +5,106 @@
 // application directly as js-polykey
 
 import { program } from 'commander'
+import inquirer from 'inquirer'
 import PolyKey from '../src/Polykey'
 import chalk from 'chalk'
+import os from 'os'
 
-let km = new PolyKey.KeyManager()
-// km.generateKeyPair('Robert Cronin', 'robert@robert.com', '99e8ervv23f2').then((key) => {
-//     console.log(key);
-// })
-let pk = new PolyKey(km, Buffer.from('some password'))
+let pk: PolyKey
+// Write a `file`
+let tempDir: string
 /*******************************************/
-program
+// initialization
+function initPolyKey(
+    password: string,
+    polykeyPath: string | undefined = undefined,
+    publicKeyPath: string | undefined = undefined,
+    privateKeyPath: string | undefined = undefined,
+    privatePassphrase: string | undefined = undefined,
+    verbose: boolean | undefined = undefined
+) {
+    try {
+        let km = new PolyKey.KeyManager(polykeyPath)
+        // import keys if provided
+        if (publicKeyPath !== undefined) {
+            km.loadPublicKey(publicKeyPath)
+            if (verbose) {
+                console.log(chalk.green('public key successfully imported'))
+            }
+        }
+        if (privateKeyPath !== undefined) {
+            if (privatePassphrase !== undefined) {
+                try {
+                    km.loadPrivateKey(privateKeyPath)
+                    if (verbose) {
+                        console.log(chalk.green('private key successfully imported'))
+                    }
+                } catch (err) {
+                    console.log(chalk.red(`Failed to import private key: ${err.message}`))
+                }
+            } else {
+                console.log(chalk.red('passphrase for private key not provided'))
+            }
+        }
+        // Initialize polykey
+        pk = new PolyKey(Buffer.from(password), undefined, undefined, polykeyPath)
+        if (verbose) {
+            console.log(chalk.green(`PolyKey was initialized successfully at '${pk._polykeyPath}'`));
+        }
+        // Initialization of temp directory (for testing)
+        tempDir = pk._fs.mkdtempSync(`${os.tmpdir}/polykeytest`, undefined).toString()
+        pk._fs.writeFileSync(`${tempDir}/file`, Buffer.from('I am to be signed'))
+    } catch (err) {
+        console.log(chalk.red(`Failed to initialize polykey: ${err.message}`))
+    }
+}
+// Run initialization
+/*******************************************/
+const polykey = new program.Command()
+polykey
     .version(require('../package.json').version, '--version', 'output the current version')
 
 /*******************************************/
+// init
+const init = polykey.command('init')
+    .description('initialize polykey')
+    // .requiredOption('-p, --password <password>', 'provide the password to polykey')
+    .option('-p, --password <password>', 'provide the password to polykey')
+    .option('--public-key <publicKey>', 'provide the path to an existing public key')
+    .option('--private-key <privateKey>', 'provide the path to an existing private key')
+    .option('--private-passphrase <privatePassphrase>', 'provide the passphrase to the private key')
+    .option('--polykey-path <polykeyPath>', 'provide the polykey path. defaults to ~/.polykey')
+    .option('--verbose', 'increase verbosity by one level')
+    .action(async (options) => {
+        const password: string = options.password
+        const polykeyPath: string | undefined = options.polykeyPath
+        const publicKeyPath: string | undefined = options.publicKey
+        const privateKeyPath: string | undefined = options.privateKey
+        const privatePassphrase: string | undefined = options.privatePassphrase
+        const verbose: boolean | undefined = options.verbose
+        initPolyKey(
+            password,
+            polykeyPath,
+            publicKeyPath,
+            privateKeyPath,
+            privatePassphrase,
+            verbose
+        )
+    })
+
+
+
+/*******************************************/
+// keymanager create new keypair
+const keymanager = polykey.command('keymanager')
+    .description('manipulate the keymanager')
+
+
+/*******************************************/
 // secrets
-const secrets = program.command('secrets')
+const secrets = polykey.command('secrets')
     .description('manipulate vault secrets')
+
     
 /*******************************************/
 // secrets list
@@ -52,9 +136,10 @@ const secretsList = secrets.command('list')
                 })
             }
         } catch (err) {
-            console.log(err.message)
+            console.log(`Failed to list secrets for vault '${vaultName}': ${err.message}`)
         }
     })
+
 
 /*******************************************/
 // secrets add
@@ -88,10 +173,12 @@ const secretAdd = secrets.command('add')
             console.log(err.message)
         }
     })
+
+
 /*******************************************/
 // vault
-const vault = program.command('vault')
-.description('manipulate vaults')
+const vault = polykey.command('vault')
+    .description('manipulate vaults')
 
 /*******************************************/
 // vault list
@@ -118,6 +205,7 @@ const vaultList = vault.command('list')
     })
 
 
+
 /*******************************************/
 // vault create
 const vaultCreate = vault.command('create')
@@ -131,10 +219,11 @@ vaultCreate
                 await pk.createVault(vaultName)
                 console.log(`vault created at ${pk._polykeyPath}/${vaultName}`);
             } catch (err) {
-                console.log(`Failed to creat vault ${vaultName}: ${err.message}`)
+                console.log(`Failed to create vault ${vaultName}: ${err.message}`)
             }
         }
     })
+
 
 /*******************************************/
 // vault destroy
@@ -143,16 +232,21 @@ vaultDestroy
     .description('destroy an existing vault')
     .option('-n, --vault-name <vaultName>', 'name of vault')
     .option('--all', 'remove all vaults')
+    .option('--verbose', 'increase verbosity by one level')
     .action(async (options) => {
+        const verbose: boolean = options.verbose ?? false
+        const deleteAll: boolean = options.all ?? false
         try {
-            if (options.all) {
+            if (deleteAll) {
                 const vaultList = pk.listVaults()
                 if (vaultList === undefined || vaultList.length == 0) {
                     console.log('no vaults found')
                 } else {
                     for (const vaultName of vaultList) {
                         await pk.destroyVault(vaultName)
-                        console.log(`destroyed ${vaultName}`)
+                        if (verbose) {
+                            console.log(chalk.green(`destroyed ${vaultName}`))
+                        }
                     }
                     console.log(chalk.green('all vaults destroyed successfully'))
                 }
@@ -228,6 +322,3 @@ if (key) {
             }
         })
 }
-
-// allow commander to parse `process.argv`
-program.parse(process.argv);
