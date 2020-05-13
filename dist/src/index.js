eb6b9602c418e7c0b191fd5039b6f6459740dd02
Robert Cronin <robert.cronin@gatech.edu>
2020-05-13 11:50:46 +0800

    WIP: add dist to gitignore

--- dist/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Polykey_js_1 = require("./Polykey.js");
-exports.default = Polykey_js_1.default;
-//# sourceMappingURL=index.js.map
